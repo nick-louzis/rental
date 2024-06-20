ca23dd104019dfbfb6c6edfb4b3ac8c152a9a2a9
Maria Magdalini <momoloca42@gmail.com>
2024-06-20 16:10:06 +0300

    init commit

---
+++ scramble.js
@@ -0,0 +1,74 @@
+class TextScramble {
+    constructor(target) {
+      this.target = target
+      this.chars = '!<>-_\\/[]{}—=+*^?#________'
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.target.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.target.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+  
+  
+  const phrases = [
+    'Ready to embark on your next adventure?',
+    'Experience Premium Bike and Car Rentals',
+    'Ride Beyond Limits',
+    'Find Your Perfect Ride'
+  ]
+  
+  const target = document.querySelector('#hook');
+  const fx = new TextScramble(target)
+  
+  let counter = 0
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 800)
+    })
+    counter = (counter + 1) % phrases.length
+  }
+  
+  next()
---
+++ script.js
@@ -0,0 +1,37 @@
+class CustomHeader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=`<header>
+        <div id="header">
+            <div><img src="" alt=""></div>
+            <nav id="nav"><ul id="navlinks">
+                <li><a href="Sercives.html">Services</a> </li>
+                <li><a href="About.html">About</a></li>
+                <li><a href="Fleet.html">Fleet</a></li>
+                <button id="mobileInner">X</button>
+            </ul>
+            
+            </nav>
+            <button id="mobile">X</button>
+        </div>
+
+    </header>`;
+    }
+}
+
+customElements.define("custom-header",CustomHeader)
+
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    const menuButtonInner = document.getElementById('mobileInner');
+    const navlinks = document.getElementById('nav');
+    menuButtonInner.addEventListener('click',function(){
+        navlinks.style.left='100%';
+     })
+
+    // const menuButton = document.getElementById('mobile');
+    // menuButton.addEventListener('click',function(){
+    //     navlinks.style.left='-100%';
+    //  })
+
+})
